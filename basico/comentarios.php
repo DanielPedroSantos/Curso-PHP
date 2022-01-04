5d7fa363fc9546166bbf35062205b74222b7a12b
Daniel Santos <55459664+DanielPedroSantos@users.noreply.github.com>
2022-01-04 01:10:10 -0300

    .

---
+++ basico/comentarios.php
@@ -0,0 +1,17 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+echo "estou no PHP";
+// echo "Comentário";
+# echo "Comentário";
+// ?>
+
+<p>Despois do priemrio bloco</p>
+
+<?php
+/*
+    Linha 1 do Comentário
+    Linha 2 do Comentário
+    Linha 3 do Comentário
+*/
+?>
